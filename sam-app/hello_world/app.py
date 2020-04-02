95683e996a186d6b05d34e9b07aeded3cef7741e
BASIC\suyama <daichi.suyama@basic.co.jp>
2020-04-02 15:26:26 +0900

    レスポンス作成

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -1,32 +1,16 @@
 import json
+import boto3  # AWS SDK for Python(PythonでAWSのリソースを操作するライブラリ)
 
-# import requests
+DYNAMO_DB = boto3.client('dynamodb')  # dynamoDBを使う
+TABLE_NAME = "trial"  # 操作したいdynamoDBのテーブル名
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
+    result = DYNAMO_DB.get_item(TableName=TABLE_NAME, Key={
+        'rid': {'N': "0"}
+    })['Item']
     # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # ip = requests.get("http://checkip.amazonaws.com/")
     # except requests.RequestException as e:
     #     # Send some context about this error to Lambda Logs
     #     print(e)
@@ -36,7 +20,9 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "id": result['sid']['S'],
+            "mmse": 0,
+            "mmsejv": "認知症判定値",
+            "mmsecm": "認知症コメント"
         }),
     }
