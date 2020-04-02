0604fa57974bf2b7b89c25a98e14e4f05cee12c9
BASIC\suyama <daichi.suyama@basic.co.jp>
2020-04-02 16:25:23 +0900

    医光モックAPIにリクエスト

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -1,14 +1,21 @@
 import json
 import boto3  # AWS SDK for Python(PythonでAWSのリソースを操作するライブラリ)
+import requests
 
 DYNAMO_DB = boto3.client('dynamodb')  # dynamoDBを使う
 TABLE_NAME = "trial"  # 操作したいdynamoDBのテーブル名
+IKO_API_ENDPOINT = "https://4k86cadch8.execute-api.ap-northeast-1.amazonaws.com/Stage/hello"
 
 
 def lambda_handler(event, context):
     result = DYNAMO_DB.get_item(TableName=TABLE_NAME, Key={
         'rid': {'N': "0"}
     })['Item']
+
+    iko_response = requests.get(IKO_API_ENDPOINT)
+
+    print(iko_response.json())
+
     # try:
     # ip = requests.get("http://checkip.amazonaws.com/")
     # except requests.RequestException as e:
